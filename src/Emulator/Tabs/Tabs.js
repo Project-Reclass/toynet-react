257eab43d1b59b5d901ba40fdd92095057503ef9
Sammy Tran <sammytran277@gmail.com>
2020-07-21 14:43:08 -0400

    Restructure wireframe based on Cecil's design

--- src/Emulator/Tabs/Tabs.js
+++ src/Emulator/Tabs/Tabs.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tabs.css';
+import Configure from './Configure/Configure';
+import Console from './Console/Console';
+import History from './History/History';
 
 const Tabs = () => {
+  const [active, setActive] = useState("configure");
+
+  const changeTabHandler = tabName => {
+    setActive(tabName);
+  }
+
+  let activeComponent = <Configure />; 
+  if (active === "console") {
+    activeComponent = <Console />;
+  } else if (active === "history") {
+    activeComponent = <History />;
+  }
+
   return (
     <div className="tabs">
-      I am the Tabs Panel!
+      <div className="tabs-container" style={{display: "flex"}}>
+        <div 
+          className={(active === "configure" ? "tab active" : "tab")}
+          onClick={() => changeTabHandler("configure")}
+        >
+            Configure
+        </div>
+        <div 
+          className={(active === "console" ? "tab active" : "tab")}
+          onClick={() => changeTabHandler("console")}
+        >
+          Console
+        </div>
+        <div 
+          className={(active === "history" ? "tab active" : "tab")}
+          onClick={() => changeTabHandler("history")}
+        >
+          History
+        </div>
+      </div>
+      {activeComponent}
     </div>
   );
 };
