96fbe25abcc75213f9f60ae1fcadb0da31ffeb59
Yi Yang <yiyang456@gmail.com>
2020-08-11 14:09:10 -0400

    updated visuals

--- src/Emulator/Visuals/Visuals.js
+++ src/Emulator/Visuals/Visuals.js
@@ -9,23 +9,24 @@ const Visuals = () => {
   const containerWidth = '75vw';
   const additionalImageSpace = 21;
   const hundred = 100;
+  const demo1 = require('./demo1.png')
+  const demo2 = require('./demo2.png')
+  const demo3 = require('./demo3.png')
+  const demo = {demo1, demo2, demo3}
 
   const [zoom, setZoom] = useState(true);
   const [hideImage, setHideImage] = useState(false);
   const imageRef = useRef();
   const [containerDimensions, setContainerDimensions] = useState({height: containerHeight, width: containerWidth});
   const [contentDimensions, setContentDimensions] = useState({height: '125px', width: '125px'});
-  const [image, setImage] = useState('null');
+  const [image, setImage] = useState(demo);
   const [, setSrc] = useState('');
   const [, setDrags] = useState(0);
-  const defaultX = 550;
-  const defaultY = 180;
-  const [pos, setPos] = useState({x: defaultX, y: defaultY});
   const [handleDetect, setHandleDetect] = useState({x: '', y: ''});
-  const [elementBounds, setElementBounds] = useState({bottom: contentDimensions.height, right: contentDimensions.width});
   const [reset, setReset] = useState(false);
   const [grabbed, setGrabbed] = useState(false);
 
+  // Conversion functions for pixel to viewport and vice versa
   function convert2Px(containerDimensions) {
     const movableSpaceReg = /([\d]+)+/g;
     const movableSpaceStringH = containerDimensions.height.toString();
@@ -37,10 +38,38 @@ const Visuals = () => {
     return {movableSpacePxH, movableSpacePxW};
   }
 
+  function pxStrip(pxElement) {
+    const noPx = /([\d]+)+/g;
+    const pxStringH = pxElement.height.toString();
+    const pxStringW = pxElement.width.toString();
+    const noPxH = parseInt(pxStringH.match(noPx)[0]);
+    const noPxW = parseInt(pxStringW.match(noPx)[0]);
+    return {noPxH, noPxW}
+  }
+
+  // Gets center of image space and centers element to the middle on render
+  function getCenter() {
+    const pxContainerDimensions = convert2Px(containerDimensions);
+    const pxContentDimensions = pxStrip(contentDimensions);
+    const centerHeight = (pxContainerDimensions.movableSpacePxH - pxContentDimensions.noPxH) / 2;
+    const centerWidth = (pxContainerDimensions.movableSpacePxW - pxContentDimensions.noPxW) / 2;
+    return {centerHeight, centerWidth}
+  }
+
+  const onRender = getCenter()
+
+  const defaultX = onRender.centerWidth;
+  const defaultY = onRender.centerHeight;
+
+  const [pos, setPos] = useState({x: defaultX, y: defaultY});
+  const [elementBounds, setElementBounds] = useState({bottom: contentDimensions.height, right: contentDimensions.width});
+
   useEffect(() => {
     const movableSpace = convert2Px(containerDimensions);
     const elementBot = parseInt(handleDetect.y) + parseInt(elementBounds.bottom) + additionalImageSpace;
     const elementRight = parseInt(handleDetect.x) + parseInt(elementBounds.right) + additionalImageSpace;
+
+    console.log(elementBot, movableSpace.movableSpacePxH, elementBounds.bottom)
 
     function convert2Vh(movableSpace) {
       const movableSpaceH = movableSpace / window.innerHeight * hundred + 'vh';
@@ -104,7 +133,7 @@ const Visuals = () => {
       const newMovableSpace = convert2Vw(addMovableSpace);
       return newMovableSpace;
     }
-  }, [handleDetect, containerDimensions, elementBounds, contentDimensions, pos, reset]);
+  }, [handleDetect, containerDimensions, elementBounds, contentDimensions, pos, reset, defaultX, defaultY]);
 
   // Handles dragging
   const handleDrag = (e, ui) => {
@@ -159,28 +188,40 @@ const Visuals = () => {
 
   // Zoom in function and adjusts drag handler size (the invisible space you can click and start drag with)
   function plusClick() {
-    const zoomAmount = .1;
-    setZoom(zoom => zoom + zoomAmount);
-    setContentDimensions({
+    if (hideImage === true) {
+      return null
+    } else {
+      const zoomAmount = .1;
+      setZoom(zoom => zoom + zoomAmount);
+      setContentDimensions({
       height: contentDimensions.height + additionalImageSpace,
       width: contentDimensions.width + additionalImageSpace,
-    });
-    elementBoundsAdjust();
-  }
+      });
+      elementBoundsAdjust();
+      }
+    }
 
   // Zoom out function and adjusts drag handler size (the invisible space you can click and start drag with)
   function minusClick() {
-    const zoomAmount = .1;
-    setZoom(zoom => zoom - zoomAmount);
-    setContentDimensions({
-      height: contentDimensions.height - additionalImageSpace,
-      width: contentDimensions.width - additionalImageSpace,
-    });
-    elementBoundsAdjust();
+    if (hideImage === true) {
+      return null
+    } else {
+      const zoomAmount = .1;
+      setZoom(zoom => zoom - zoomAmount);
+      setContentDimensions({
+      height: parseInt(contentDimensions.height) - additionalImageSpace,
+      width: parseInt(contentDimensions.width) - additionalImageSpace,
+      });
+      elementBoundsAdjust();
+    }
   }
 
   function recenterClick() {
-    setReset(!reset);
+    if (hideImage === true){
+      return null
+    } else {
+      setReset(!reset);
+    }
   }
 
   function hideClick() {
@@ -188,7 +229,14 @@ const Visuals = () => {
   }
 
   return (
-  <div>
+  <div style={{position: 'relative'}}>
+    {/* Functional buttons that changes the element inside the container */}
+    <div className="icons">
+        <button className='iconButtons' onClick={plusClick} style={{cursor: hideImage ? 'default' : 'pointer'}}><FontAwesomeIcon className='icon' icon={faSearchPlus} /></button>
+        <button className='iconButtons' onClick={minusClick} style={{cursor: hideImage ? 'default' : 'pointer'}}><FontAwesomeIcon className='icon' icon={faSearchMinus} /></button>
+        <button className='iconButtons' onClick={recenterClick} style={{cursor: hideImage ? 'default' : 'pointer'}}><FontAwesomeIcon className='icon' icon={faUndo} /></button>
+        <button className='iconButtons' onClick={hideClick}><FontAwesomeIcon className='icon' icon={faEyeSlash} /></button>
+    </div>
     {/* Sets container size, space you can drag in container, and the element inside the container's size */}
     <div className="imageContainer" style={{height: containerHeight, width: containerWidth, overflow: 'auto', padding: '0px'}}>
       <div className="imageSpace" style={{height: containerDimensions.height || '49vh', width: containerDimensions.width || '74vw'}}>
@@ -207,9 +255,9 @@ const Visuals = () => {
             cursor: grabbed ? '-webkit-grabbing': ''}}>
             {/* Functional buttons that changes the element inside the container */}
             <div>
-                <button onClick={() => setImage('https://picsum.photos/200')}>Image 200px</button> <br></br>
-                <button onClick={() => setImage('https://picsum.photos/300')}>Image 300px</button> <br></br>
-                <button onClick={() => setImage('https://picsum.photos/400')}>Image 400px</button> <br></br>
+                <button onClick={() => setImage(demo.demo1)}>Demo 1</button> <br></br>
+                <button onClick={() => setImage(demo.demo2)}>Demo 2</button> <br></br>
+                <button onClick={() => setImage('https://picsum.photos/200')}>Demo 3</button> <br></br>
                 <img
                   className="image"
                   style={{
@@ -223,13 +271,6 @@ const Visuals = () => {
             </div>
           </div>
         </Draggable>
-      </div>
-      {/* Functional buttons that changes the element inside the container */}
-      <div className="icons">
-        <button className='iconButtons' onClick={plusClick}><FontAwesomeIcon className='icon' icon={faSearchPlus} /></button>
-        <button className='iconButtons' onClick={minusClick}><FontAwesomeIcon className='icon' icon={faSearchMinus} /></button>
-        <button className='iconButtons' onClick={recenterClick}><FontAwesomeIcon className='icon' icon={faUndo} /></button>
-        <button className='iconButtons' onClick={hideClick}><FontAwesomeIcon className='icon' icon={faEyeSlash} /></button>
       </div>
     </div>
   </div>
