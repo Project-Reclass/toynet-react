07167052281eefdff6e3b87d88376413b0eed16b
Yi Yang <yiyang456@gmail.com>
2020-08-14 16:04:36 -0400

    refined image panel

--- src/Emulator/Instructions/Instructions.tsx
+++ src/Emulator/Instructions/Instructions.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder, faUser } from '@fortawesome/free-solid-svg-icons';
 import './Instructions.css';
 
 export interface PanelData {
@@ -15,14 +17,24 @@ interface Props {
 const Instructions: FC<Props> = ({ panelData }) => {
   return (
     <div className="instructions">
-      <div className="icon-container">icon1 icon2 ...</div>
+      <div className="icon-container">
+        <button className='iconButtons'>
+          <FontAwesomeIcon className='icons' size='lg' icon={faFolder} />
+        </button>
+        <button className='iconButtons'>
+          <FontAwesomeIcon className='icons' icon={faUser} />
+        </button>
+        <div className="line">&nbsp;</div>
+      </div>
       <div className="title-container">
         <h4 className="module-num">Module {panelData.submoduleNumber}</h4>
         <p className="module-name">{panelData.submoduleName}</p>
+        <div className="line">&nbsp;</div>
       </div>
       <div className="objective-container">
         <h4 className="objective-title">Objective</h4>
         <p className="objective">{panelData.objective}</p>
+        <div className="line">&nbsp;</div>
       </div>
       <div>
         <h4 className="tasks-title">Tasks</h4>
@@ -31,7 +43,7 @@ const Instructions: FC<Props> = ({ panelData }) => {
             <li key={`${task}${idx}`}>{task}</li>
           ))}
         </ol>
-      </div>
+      </div>    
     </div>
   );
 };
