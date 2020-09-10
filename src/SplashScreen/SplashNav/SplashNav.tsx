64f35b8f8d40fc26c7d862af5b27b9e5ccd7202c
Yi Yang <yiyang456@gmail.com>
2020-09-10 16:14:08 -0400

    changed file/folder names to match design breakup

--- src/SplashScreen/SplashNav/SplashNav.tsx
+++ src/SplashScreen/SplashNav/SplashNav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './SplashHeader.css';
+import './SplashNav.css';
 
 function SplashHeader() {
   return (
--- src/SplashScreen/SplashScreen.tsx
+++ src/SplashScreen/SplashScreen.tsx
@@ -1,13 +1,13 @@
 import React, {FC} from 'react';
 
 import './SplashScreen.css';
-import SplashHeader from './SplashHeader/SplashHeader';
+import SplashNav from './SplashNav/SplashNav';
 
 
 const Splashscreen: FC = () => {
   return (
     <div className="splash-screen">
-      <SplashHeader />
+      <SplashNav />
       {/* <div>
         <a  style={{marginLeft: '1000px'}} href='/module/0/emulator/0'>Visit Emulator</a> <br />
         <a href='/module'>Visit Courses</a>
