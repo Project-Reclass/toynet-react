04c57ace0dfbc58cd458f93a1c6ee459dc8df770
Yi Yang <yiyang456@gmail.com>
2020-09-11 18:47:53 -0400

    header fix

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Header.css';
 import {ReactComponent as FolderIcon} from '../assets/headerIcons/folderIcon.svg';
@@ -7,7 +7,7 @@ import {ReactComponent as HelpIcon} from '../assets/headerIcons/helpIcon.svg';
 import {ReactComponent as LogIcon} from '../assets/headerIcons/logIcon.svg';
 
 const Header = () => {
-  const history = useHistory()
+  const history = useHistory();
   const [menu, setMenu] = useState(false);
   const [enableHref, setEnableHref] = useState(false);
 
@@ -16,14 +16,12 @@ const Header = () => {
     setEnableHref(prevEnableHref => !prevEnableHref);
   }
 
-  function routeChange(path: any) {
-    const path2='blank'
-    history.push(path2)
-  }
-
-  function blank() {
-    console.log('placeholder')
-  }
+  const goToPageOnEnableHref = (path: string) => {
+    if (enableHref) {
+      console.log({ path });
+      history.push(path);
+    }
+  };
 
   const linkTextStyle = {
     visibility: menu ? 'visible' : '',
@@ -34,51 +32,50 @@ const Header = () => {
 
   return (
     <nav className='fixed-sidebar' onClick={toggleMenu}
-    style={{
-      width: menu ? '14rem' : '',
-    }}>
+      style={{
+        width: menu ? '14rem' : '',
+      }}
+    >
       <ul className='navbar-nav'>
         <li className='logo'>
-          <div className='nav-icon'>
+          <div className='nav-icon' onClick={() => goToPageOnEnableHref('/blank')}>
             {/* Temporarily. Needs a logo svg */}
             <span className='svg'>X</span>
-            <a href='blank' className='link-text' style={linkTextStyle}>
+            <span className='link-text' style={linkTextStyle}>
               Logo
-            </a>
+            </span>
           </div>
         </li>
-        
-        <div className='divset-2' >
-        <li className='nav-item' onClick={enableHref ? blank : routeChange}>
+        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
           <div className='nav-icon'>
             <span className='svg'> <FolderIcon className='svg-icon' /> </span>
-            <a href='blank' className='link-text' style={linkTextStyle}>
+            <span className='link-text' style={linkTextStyle}>
               Curriculum
-            </a>
+            </span>
           </div>
-
         </li>
-        </div>
-        <li className='nav-item'>
+        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
           <div className='nav-icon'>
             <span className='svg'> <AccountIcon /> </span>
-            <a href='blank' className='link-text' style={linkTextStyle}>Profile</a>
+            <span className='link-text' style={linkTextStyle}>
+              Profile
+            </span>
           </div>
         </li>
-        <li className='nav-item'>
+        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
           <div className='nav-icon'>
             <span className='svg'> <HelpIcon /> </span>
-            <a href='blank' className='link-text' style={linkTextStyle}>
+            <span className='link-text' style={linkTextStyle}>
               FAQ
-            </a>
+            </span>
           </div>
         </li>
-        <li className='nav-item'>
+        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
           <div className='nav-icon'>
             <span className='svg'> <LogIcon /> </span>
-            <a href='blank' className='link-text' style={linkTextStyle}>
+            <span className='link-text' style={linkTextStyle}>
               Log Out
-            </a>
+            </span>
           </div>
         </li>
       </ul>
