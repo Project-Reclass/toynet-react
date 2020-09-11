5742c02aa79a614f3da40c75f4c21e07654c1679
Yi Yang <yiyang456@gmail.com>
2020-09-11 19:46:13 -0400

    final 3

--- src/SplashScreen/SplashScreen.tsx
+++ src/SplashScreen/SplashScreen.tsx
@@ -8,6 +8,7 @@ const Splashscreen: FC = () => {
   return (
     <div className="splash-screen">
       <SplashNav />
+      {/* temporary div below is needed for reference to getting to emulator */}
       {/* <div>
         <a  style={{marginLeft: '1000px'}} href='/module/0/emulator/0'>Visit Emulator</a> <br />
         <a href='/module'>Visit Courses</a>
