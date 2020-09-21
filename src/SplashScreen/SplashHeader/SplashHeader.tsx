f829c94027f7579505fc2a0ec206905b94d93b82
Scott Richardson <scottjr632@gmail.com>
2020-09-21 11:02:12 -0500

    Changes button to try it out

--- src/SplashScreen/SplashHeader/SplashHeader.tsx
+++ src/SplashScreen/SplashHeader/SplashHeader.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './SplashHeader.css';
 import {ReactComponent as Illustration} from '../../assets/splashScreen/illustration1.svg';
+import { useHistory } from 'react-router-dom';
 
 function SplashHeader() {
+  const history = useHistory();
+
+  const goToEmulator = () => history.push('/module/1/emulator/1');
+
   return (
     <div className='spash-header__container '>
       <div className='splash-header-body container-1920 mx-auto' id="home">
@@ -15,8 +20,8 @@ function SplashHeader() {
           <h3>
             learn anywhere, anytime.
           </h3>
-          <button className='how-it-works'>
-            <h3 className='splash-button-text'>How it works</h3>
+          <button className='how-it-works' onClick={goToEmulator}>
+            <h3 className='splash-button-text'>Try it out!</h3>
           </button>
         </div>
         <div className='splash-header-image-container'>
