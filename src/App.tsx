6382ba8bb831215bb179e06bb1ed9a0ff0567a7b
Scott Richardson <scottjr632@gmail.com>
2020-09-27 21:18:54 -0500

    Global CSS optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 import Emulator from './Emulator';
 import Header from './Header/Header';
 import ModuleList from './ModuleList';
@@ -10,9 +9,6 @@ import Article from './ModuleList/Article';
 import Quiz from './ModuleList/Quiz';
 import SplashScreen from './SplashScreen';
 import { useFeatureFlags } from './FeatureFlags';
-
-// rename App.js and App.css to navbar component
-// Move navbar.js, navbar.css, and logo(?) once file structure is determined
 
 const data = {
   'id': 1,
