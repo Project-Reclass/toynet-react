a55bb29df441b8bc8538cb141665043798d4af35
Scott Richardson <scottjr632@gmail.com>
2020-10-17 12:34:12 -0500

    wip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+currURI=https://raw.githubusercontent.com/Project-Reclass/toynet-django/master
 if [ -z ${SERVER_URI+x} ]; then echo "SERVER_URI needs to be set"; exit 1 ; else echo "SERVER_URI is set to '$SERVER_URI'"; fi
 
-envsubst '\$SERVER_URI' < http.conf > /etc/nginx/conf.d/default.conf
+if [ -z ${CURR_URI} ]; then echo "CURR_URI not set. Defaulting to $currURI" ; else set $CURR_URI=$currURI; echo "CURR_URI is et to '$currURI'"; fi
+
+echo $CURR_URI
+
+envsubst '\${CURR_URI},\${SERVER_URI}' < http.conf > /etc/nginx/conf.d/default.conf
+
+cat /etc/nginx/conf.d/default.conf
 
 nginx -t
 exec nginx -g 'daemon off;'
