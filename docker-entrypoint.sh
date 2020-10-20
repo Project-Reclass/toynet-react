d66e5d14c1c56b1e41e3b8097044970860aad0e2
Scott Richardson <scottjr632@gmail.com>
2020-10-20 10:32:52 -0500

    Allows for variable CURR_URIs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-currURI=https://raw.githubusercontent.com/Project-Reclass/toynet-django/master
+defaultCurrURI=https://raw.githubusercontent.com/Project-Reclass/toynet-django/master
+
 if [ -z ${SERVER_URI+x} ]; then echo "SERVER_URI needs to be set"; exit 1 ; else echo "SERVER_URI is set to '$SERVER_URI'"; fi
 
-if [ -z ${CURR_URI} ]; then echo "CURR_URI not set. Defaulting to $currURI" ; else set $CURR_URI=$currURI; echo "CURR_URI is et to '$currURI'"; fi
-
-echo $CURR_URI
+if [ -z ${CURR_URI} ]; then echo "CURR_URI not set. Defaulting to $defaultCurrURI"; export CURR_URI=${defaultCurrURI}; else echo "CURR_URI is et to '$defaultCurrURI'"; fi
 
 envsubst '\${CURR_URI},\${SERVER_URI}' < http.conf > /etc/nginx/conf.d/default.conf
-
-cat /etc/nginx/conf.d/default.conf
 
 nginx -t
 exec nginx -g 'daemon off;'
