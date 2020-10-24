4f265debc837c8d69a8c2002dd54c925f32c67ee
SungYong Chung <sc4286@columbia.edu>
2020-10-23 22:08:59 -0400

    style header using styled components

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,7 +6,7 @@ import {ReactComponent as AccountIcon} from '../assets/headerIcons/accountIcon.s
 import {ReactComponent as HelpIcon} from '../assets/headerIcons/helpIcon.svg';
 import {ReactComponent as LogIcon} from '../assets/headerIcons/logIcon.svg';
 
-import './Header.css';
+import styled from '@emotion/styled';
 
 const Header = () => {
   const history = useHistory();
@@ -24,63 +24,138 @@ const Header = () => {
     }
   };
 
-  const linkTextStyle = {
-    visibility: menu ? 'visible' : '',
-    opacity: menu ? '1' : '',
-    transition: 'opacity 200ms',
-    whiteSpace: 'nowrap',
-  } as React.CSSProperties;
+
+
+const width = menu ? '14rem' : '3rem';
+const StyledNav = styled.nav`
+  z-index: 3;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  background: rgb(24, 21, 21);
+  transition: width 200ms ease;
+  width: ${width};
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
+const StyledLogo = styled.li`
+  font-weight: bold;
+  text-transform: uppercase;
+  position: relative;
+  top: 0;
+  margin-bottom: 15rem;
+  color: red;
+`;
+
+const StyledNavItem = styled.li`
+  margin-top: auto;
+
+  &:hover {
+    color: white;
+    border-left: solid 3px green;
+  }
+`;
+
+const StyledNavIcon = styled.div`
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  text-decoration: none;
+  cursor: pointer;
+
+  stroke-width: 2;
+  stroke: grey;
+  opacity: 0.5;
+  transition: opacity 100ms;
+
+  &:hover {
+    stroke-width: 2.5;
+    stroke: #fff;
+    opacity: 0.95;
+  }
+`;
+
+const StyledSvg = styled.span`
+  margin: 0 0.35rem;
+
+  &:hover {
+   cursor: pointer;
+  }
+`;
+
+const visibility = menu ? 'visible' : '';
+const opacity = menu ? '1' : '';
+const StyledLinkText = styled.span`
+  color: white;
+  filter: grayscale(100%) opacity(0.7);
+  transition: color 200ms;
+
+  transition: opacity 200ms;
+  whiteSpace: nowrap;
+  visibility: ${visibility};
+  opacity: ${opacity};
+
+  &:hover {
+    filter: grayscale(0%) opacity(1);
+    text-decoration: none;
+    color: white;
+  }
+`;
 
   return (
-    <nav className='fixed-sidebar' onClick={toggleMenu}
-      style={{
-        width: menu ? '14rem' : '',
-      }}
-    >
-      <ul className='navbar-nav'>
-        <li className='logo'>
-          <div className='nav-icon' onClick={() => goToPageOnEnableHref('/blank')}>
+    <StyledNav onClick={toggleMenu}>
+      <ul>
+        <StyledLogo>
+          <StyledNavIcon onClick={() => goToPageOnEnableHref('/blank')}>
             {/* Temporarily. Needs a logo svg */}
-            <span className='svg'>X</span>
-            <span className='link-text' style={linkTextStyle}>
+            <StyledSvg>X</StyledSvg>
+            <StyledLinkText>
               Logo
-            </span>
-          </div>
-        </li>
-        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
-          <div className='nav-icon'>
-            <span className='svg'> <FolderIcon className='svg-icon' /> </span>
-            <span className='link-text' style={linkTextStyle}>
-              Curriculum
-            </span>
-          </div>
-        </li>
-        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
-          <div className='nav-icon'>
-            <span className='svg'> <AccountIcon /> </span>
-            <span className='link-text' style={linkTextStyle}>
+            </StyledLinkText>
+          </StyledNavIcon>
+        </StyledLogo>
+        <StyledNavItem onClick={() => goToPageOnEnableHref('/blank')}>
+          <StyledNavIcon>
+            <StyledSvg> <FolderIcon className='svg-icon' /> </StyledSvg>
+            <StyledLinkText>
+              Curriculumm
+            </StyledLinkText>
+          </StyledNavIcon>
+        </StyledNavItem>
+        <StyledNavItem onClick={() => goToPageOnEnableHref('/blank')}>
+          <StyledNavIcon>
+            <StyledSvg> <AccountIcon /> </StyledSvg>
+            <StyledLinkText>
               Profile
-            </span>
-          </div>
-        </li>
-        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
-          <div className='nav-icon'>
-            <span className='svg'> <HelpIcon /> </span>
-            <span className='link-text' style={linkTextStyle}>
+            </StyledLinkText>
+          </StyledNavIcon>
+        </StyledNavItem>
+        <StyledNavItem onClick={() => goToPageOnEnableHref('/blank')}>
+          <StyledNavIcon>
+            <StyledSvg> <HelpIcon /> </StyledSvg>
+            <StyledLinkText>
               FAQ
-            </span>
-          </div>
-        </li>
-        <li className='nav-item' onClick={() => goToPageOnEnableHref('/blank')}>
-          <div className='nav-icon'>
-            <span className='svg'> <LogIcon /> </span>
-            <span className='link-text' style={linkTextStyle}>
+            </StyledLinkText>
+          </StyledNavIcon>
+        </StyledNavItem>
+        <StyledNavItem onClick={() => goToPageOnEnableHref('/blank')}>
+          <StyledNavIcon>
+            <StyledSvg> <LogIcon /> </StyledSvg>
+            <StyledLinkText>
               Log Out
-            </span>
-          </div>
-        </li>
+            </StyledLinkText>
+          </StyledNavIcon>
+        </StyledNavItem>
       </ul>
-    </nav>
+    </StyledNav>
   );
 };
 
