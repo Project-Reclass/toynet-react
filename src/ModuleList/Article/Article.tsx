3a35c46449160e3e6ca3e1792a0d78e34404650a
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2020-11-05 08:27:34 -0800

    fixed testing errors for splash article render

--- src/ModuleList/Article/Article.tsx
+++ src/ModuleList/Article/Article.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { text } from './mockData';
 import ReactMarkdown from 'react-markdown';
---
+++ src/ModuleList/Article/ArticleStyles.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const MarkdownWrapper = styled('h3')`
+  color: white;
+  width: 80%;
+  line-height: 1.5rem;
+  p {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const HeadingContainer = styled('h1')`
+  color: white;
+  width: 80%;
+  margin-bottom: 1rem;
+
+`;
+
+export const AuthorContainer = styled('h1')`
+  color: white;
+  width: 80%;
+  margin-bottom: 1rem;
+
+`;
+
+
+export const Div = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
