e56443e2e46f9a6063febe76d6b6fbf12007928e
Scott Richardson <scottjr632@gmail.com>
2020-12-13 21:35:17 -0600

    Fixes tests

--- src/__tests__/ModuleList/Module/SubModule/SubModule.test.jsx
+++ src/__tests__/ModuleList/Module/SubModule/SubModule.test.jsx
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react'
 
 import SubModule from 'src/Curriculum/Module/SubModule/SubModule';
+import { renderTreeWithTheme, renderWithTheme } from 'src/common/test-utils/renderWithTheme';
 
 const defaultProps = {
   title: 'Article',
@@ -12,18 +13,33 @@ const defaultProps = {
   type: 'article',
 }
 
+const valuesProps = {
+  title: 'Article',
+  progress: 99,
+  id: 1,
+  moduleId: 2,
+  type: 'value',
+}
+
 describe('The sub modules', () => {
   it('should render and match previous snapshots', () => {
-    const tree = renderer.create(
+    const tree = renderTreeWithTheme(
       <SubModule {...defaultProps} />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('should create a link to the submodule page', () => {
-    const { moduleId, type, id, title } = defaultProps;
-    const { getByText } = render(<SubModule {...defaultProps} />);
+    const { type, id, title } = defaultProps;
+    const { getByText } = renderWithTheme(<SubModule {...defaultProps} />);
     const link = getByText(title);
 
-    expect(link.getAttribute('href')).toBe(`/module/${moduleId}/${type.toString()}/${id}`)
+    expect(link.getAttribute('href')).toBe(`/module/0/${type.toString()}/${id}`)
   });
+  it('should create a link to a values page', () => {
+    const { type, id, title } = valuesProps;
+    const { getByText } = renderWithTheme(<SubModule {...valuesProps} />);
+    const link = getByText(title);
+
+    expect(link.getAttribute('href')).toBe(`/${type.toString()}/${id}`)
+  })
 });
