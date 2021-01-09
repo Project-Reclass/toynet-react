bee5c4176ca6bdcefd1a29102d1a69ef35bfb542
Scott Richardson <scottjr632@gmail.com>
2021-01-09 00:08:58 -0600

    Adds test for the article connection

--- src/__tests__/ModuleList/Article/Article.test.jsx
+++ src/__tests__/ModuleList/Article/Article.test.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter, Route } from 'react-router-dom';
+import {act} from 'react-dom/test-utils';
+import { getByText, render, waitFor } from '@testing-library/react';
 
 import Article from 'src/Curriculum/Article';
+jest.mock('src/common/api/curriculum/article/requests');
+import { getArticleText, getArticleMeta } from 'src/common/api/curriculum/article/requests';
+
+const articleId = 42;
+
+const mockArticle = {
+  text: 'I have the high ground!',
+  meta: {
+    source: 'https://obiwan.gov',
+    title: 'Return of the Sith',
+    author: 'Obi-Wan Kenobi',
+    images: [],
+  }
+}
 
 const RenderWithRouter = ({ children, moduleId, articleId }) => (
   <MemoryRouter initialEntries={[`/module/${moduleId}/article/${articleId}`]}>
@@ -11,12 +27,48 @@ const RenderWithRouter = ({ children, moduleId, articleId }) => (
 );
 
 describe('The Article page', () => {
-  it('should render the same based on URL parameters', () => {
-    const tree = renderer.create(
-      <RenderWithRouter moduleId={42} articleId={64}>
+  beforeEach(() => {
+    getArticleText.mockClear();
+    getArticleMeta.mockClear();
+
+    getArticleMeta.mockResolvedValue(mockArticle.meta);
+    getArticleText.mockResolvedValue(mockArticle.text);
+  });
+  it('should get article text and meta for article id specified in the URL', async () => {
+    render(
+      <RenderWithRouter articleId={articleId}>
         <Article />
       </RenderWithRouter>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    );
+
+    await waitFor(() =>  expect(getArticleMeta.mock.calls[0][0]).toBe(articleId));
+    await waitFor(() => expect(getArticleText.mock.calls[0][0]).toBe(articleId));
+  });
+  it('should render the text of the article', async () => {
+    const { getByText } = render(
+      <RenderWithRouter articleId={articleId}>
+        <Article />
+      </RenderWithRouter>
+    );
+
+    await waitFor(() => expect(getByText(mockArticle.text)).toBeInTheDocument());
+  });
+  it('should render the author of the article', async () => {
+    const { getByText } = render(
+      <RenderWithRouter articleId={articleId}>
+        <Article />
+      </RenderWithRouter>
+    );
+
+    await waitFor(() => expect(getByText(mockArticle.meta.author, { exact: false })).toBeInTheDocument());
+  });
+  it('should render the title of the article', async () => {
+    const { getByText } = render(
+      <RenderWithRouter articleId={articleId}>
+        <Article />
+      </RenderWithRouter>
+    );
+
+    await waitFor(() => expect(getByText(mockArticle.meta.title)).toBeInTheDocument());
   });
 });
