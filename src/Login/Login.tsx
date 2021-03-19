938e526408c4a1d1d468e28c61bb707a2adb2ac0
Navya Ramagiri <42819448+navya825@users.noreply.github.com>
2021-03-19 19:18:29 -0400

    red borders for invalid inputs (#82)
    * red borders for invalid inputs
    
    * testing for make input red when invalid

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Box,
-  Heading,
-  Stack,
-  useToast,
-} from '@chakra-ui/core';
+import { Box, Heading, Stack, useToast } from '@chakra-ui/core';
 
 import { useAuthContext, AuthActions } from './AuthProvider';
 import { useLogin } from 'src/common/api/login/hooks';
 import { Center, ToyNetInput, ToyNetButton } from './styled';
 import PasswordInput from './PasswordInput';
+
+const minimumInputLength = 3;
 
 const Login = () => {
   const toast = useToast();
@@ -18,6 +15,8 @@ const Login = () => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameInvalid, setUsernameInvalid] = useState(false);
+  const [passwordInvalid, setPasswordInvalid] = useState(false);
 
   useEffect(() => {
     if (!isLoading && isSuccess && data) {
@@ -38,12 +37,16 @@ const Login = () => {
   }, [isError, isLoading, toast]);
 
   const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter')
-      handleLogin();
+    if (e.key === 'Enter') handleLogin();
   };
 
   const handleLogin = () => {
-    if (username.length > 2 && password.length > 2) {
+    setUsernameInvalid(username.length <= minimumInputLength);
+    setPasswordInvalid(password.length <= minimumInputLength);
+    if (
+      username.length > minimumInputLength &&
+      password.length > minimumInputLength
+    ) {
       login({ username, password });
       return;
     }
@@ -59,29 +62,33 @@ const Login = () => {
     <Box width="100%" height="100vh">
       <Center width="25rem" height="30vh">
         <form>
-        <Stack spacing={4}>
-          <Heading textAlign='center'>Login to ToyNet!</Heading>
-          <ToyNetInput
-            placeholder='Username'
-            type='text'
-            autoComplete='username'
-            onKeyPress={handleEnter}
-            value={username}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setUsername(e.currentTarget.value)}
-          />
-          <PasswordInput
-            placeholder='Password'
-            autoComplete='current-password'
-            onKeyPress={handleEnter}
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.currentTarget.value)}
-          />
-          <ToyNetButton onClick={handleLogin}>
-            Sign In
-          </ToyNetButton>
-        </Stack>
+          <Stack spacing={4}>
+            <Heading textAlign="center">Login to ToyNet!</Heading>
+            <ToyNetInput
+              placeholder="Username"
+              type="text"
+              autoComplete="username"
+              onKeyPress={handleEnter}
+              value={username}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setUsername(e.currentTarget.value)
+              }
+              isInvalid={usernameInvalid}
+              errorBorderColor="red.300"
+            />
+            <PasswordInput
+              placeholder="Password"
+              autoComplete="current-password"
+              onKeyPress={handleEnter}
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.currentTarget.value)
+              }
+              isInvalid={passwordInvalid}
+              errorBorderColor="red.300"
+            />
+            <ToyNetButton onClick={handleLogin}>Sign In</ToyNetButton>
+          </Stack>
         </form>
       </Center>
     </Box>
