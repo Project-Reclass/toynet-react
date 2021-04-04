6761eefb1a44a07defe4d05537db720524e07696
Scott Richardson <scottjr632@gmail.com>
2021-04-04 18:48:22 -0500

    Fixes type check issue

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,4 +6,19 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { ResizeObserver } from '@juggle/resize-observer';
 
+// ResizeObserver is required to be added to the `jsdom`
+// it is checked on the global object for the `flow` library
+// We add the `ResizeObserver` type here to the global node
+// so to ensure type safety.
+declare global {
+  namespace NodeJS {
+    interface Global {
+      document: Document;
+      window: Window;
+      navigator: Navigator;
+      ResizeObserver: typeof ResizeObserver;
+    }
+  }
+}
+
 global.ResizeObserver = ResizeObserver;
