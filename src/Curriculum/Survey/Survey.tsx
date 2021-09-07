73c3b441de13ee4ebcab0e4494d85aba1f5b4b4f
Josiah Aviles <josiaha@uchicago.edu>
2021-09-07 13:28:27 -0500

    cleaning code

--- src/Curriculum/Survey/Survey.tsx
+++ src/Curriculum/Survey/Survey.tsx
@@ -28,127 +28,118 @@ interface Params {
 
 const answeredIndices = new Set();
 
-const Survey = () => {
-
-    const { surveyId } = useParams<Params>();
-    const { data, isLoading } = useSurveyMeta(Number(surveyId));
-    const dataLength = data ? data.items.length : 0;
-
-    const [surveyAnswers, setSurveyAnswers] = useState<string[]>(new Array<string>(dataLength));
-
-    const UpdateSurveyFunc = (resp: string, id: number) => {
-        return () => {
-            answeredIndices.add(id);
-            surveyAnswers[id] = resp;
-            setSurveyAnswers(surveyAnswers);
-        };
-    };
-    const UpdateSurvey = (resp: string, id: number) => {
-        answeredIndices.add(id);
-        surveyAnswers[id] = resp;
-        setSurveyAnswers(surveyAnswers);
-    };/*
-
-    const RenderInput: FC<{question:SurveyQuestion, id:number}> = ({question, id}) => {
-        //console.log('Rendering Input');
-        let inputForm;
-        switch (question.item_type) {
-            case 'CHOICE':
+const RenderInput: FC<{question:SurveyQuestion, id:number, updateResp:Function}> = ({question, id, updateResp}) => {
+    let inputForm;
+    switch (question.item_type) {
+        case 'CHOICE':
+            inputForm = (
+                <SimpleGrid columns={2} spacingX={1}>
+                {question.options?.map((option) => (
+                <div key={option}>
+                    <input
+                      type="radio"
+                      id={option}
+                      data-testid={option}
+                      name={id.toString()}
+                      value={option}
+                      onChange={() => updateResp(option, id)}
+                      style={{margin: '5px'}}
+                    />
+                    <QuestionLabel
+                      as={'label'}
+                      fontSize='lg'
+                      isIncorrect={false}
+                    >
+                        {option}
+                    </QuestionLabel>
+                </div>
+                ))}
+                </SimpleGrid>
+            );
+            break;
+        case 'SCALE':
+            if (question.options) {
                 inputForm = (
-                    <SimpleGrid columns={2} spacingX={1}>
+                    <SimpleGrid columns={question.options?.length} spacingX={1}>
                     {question.options?.map((option) => (
                     <div key={option}>
+                        <QuestionLabel
+                          as={'label'}
+                          fontSize='lg'
+                          isIncorrect={false}
+                        >
+                            {option}
+                        </QuestionLabel>
+                        <br/>
                         <input
                           type="radio"
                           id={option}
                           data-testid={option}
                           name={id.toString()}
                           value={option}
-                          onChange={UpdateSurveyFunc(option, id)}
+                          onChange={() => updateResp(option, id)}
                           style={{margin: '5px'}}
                         />
-                        <QuestionLabel
-                            as={'label'}
-                            fontSize='lg'
-                            isIncorrect={false}
-                        >
-                            {option}
-                        </QuestionLabel>
                     </div>
                     ))}
                     </SimpleGrid>
                 );
-                break;
-            case 'SCALE':
-                if (question.options) {
-                    inputForm = (
-                        <SimpleGrid columns={question.options?.length} spacingX={1}>
-                        {question.options?.map((option) => (
-                        <div key={option}>
-                            <QuestionLabel
-                                as={'label'}
-                                fontSize='lg'
-                                isIncorrect={false}
-                            >
-                                {option}
-                            </QuestionLabel>
-                            <br/>
-                            <input
-                            type="radio"
-                            id={option}
-                            data-testid={option}
-                            name={id.toString()}
-                            value={option}
-                            onChange={UpdateSurveyFunc(option, id)}
-                            style={{margin: '5px'}}
-                            />
-                        </div>
-                        ))}
-                        </SimpleGrid>
-                    );
-                } else {
-                    inputForm = (
-                        <Slider
-                        aria-label="slider1"
-                        defaultValue={10}
-                        max={10}
-                        onChange={(val) => UpdateSurvey((val+1).toString(), id)}>
-                            <SliderTrack height="10px">
-                                <SliderFilledTrack height="10px" />
-                            </SliderTrack>
-                            <SliderThumb/>
-                        </Slider>
-                    );
-                };
-                break;
-            case 'TEXT':
-                if (question.unit) {
-                    inputForm = (
-                        <InputGroup>
-                            <Input
-                            onChange={(val: React.ChangeEvent<HTMLInputElement>) => UpdateSurvey(val.target.value, id)}
-                            placeholder="Write Here"
-                            color="black"/>
-                            <InputRightAddon children={question.unit} color="black"/>
-                        </InputGroup>
-                    );
-                } else {
-                    inputForm = <Input
-                    onChange={(val: React.ChangeEvent<HTMLInputElement>) => UpdateSurvey(val.target.value, id)}
-                    placeholder="Write Here"
-                    color="black"/>;
-                }
-                break;
-            case 'LONGTEXT':
-                inputForm = <Textarea
-                onChange={(val: React.ChangeEvent<HTMLInputElement>) => UpdateSurvey(val.target.value, id)}
-                placeholder="type here!"
+            } else {
+                inputForm = (
+                    <Slider
+                      aria-label="slider1"
+                      defaultValue={10}
+                      max={10}
+                      onChange={(val) => updateResp((val+1).toString(), id)}>
+                        <SliderTrack height="10px">
+                            <SliderFilledTrack height="10px" />
+                        </SliderTrack>
+                        <SliderThumb/>
+                    </Slider>
+                );
+            };
+            break;
+        case 'TEXT':
+            if (question.unit) {
+                inputForm = (
+                    <InputGroup>
+                        <Input
+                          onChange={(val: React.ChangeEvent<HTMLInputElement>) => updateResp(val.target.value, id)}
+                          placeholder="Type Here"
+                          color="black"/>
+                        <InputRightAddon children={question.unit} color="black"/>
+                    </InputGroup>
+                );
+            } else {
+                inputForm = <Input
+                onChange={(val: React.ChangeEvent<HTMLInputElement>) => updateResp(val.target.value, id)}
+                placeholder="Type Here"
                 color="black"/>;
-                break;
-            default:
-                inputForm = <p>default</p>;
-        };
-        return inputForm;
+            }
+            break;
+        case 'LONGTEXT':
+            inputForm = <Textarea
+            onChange={(val: React.ChangeEvent<HTMLInputElement>) => updateResp(val.target.value, id)}
+            placeholder="Type Here"
+            color="black"/>;
+            break;
+        default:
+            inputForm = <p>default</p>;
+    };
+    return inputForm;
+};
+
+const Survey = () => {
+
+    const { surveyId } = useParams<Params>();
+    const { data, isLoading } = useSurveyMeta(Number(surveyId));
+    const dataLength = data ? data.items.length : 0;
+    const [surveyAnswers, setSurveyAnswers] = useState<string[]>(new Array<string>(dataLength));
+
+    const updateSurvey = (resp: string, id: number) => {
+        answeredIndices.add(id);
+        surveyAnswers[id] = resp;
+        setSurveyAnswers(surveyAnswers);
     };
 
     return (
@@ -159,7 +150,7 @@ const Survey = () => {
                     {data?.items && data.items.map((q: SurveyQuestion, surveyId: number) => (
                         <div key={surveyId}>
                             <p>{surveyId + 1}. {q.question}</p>
-                            <RenderInput question={q} id={surveyId}/>
+                            <RenderInput question={q} id={surveyId} updateResp={updateSurvey}/>
                         </div>
                     ))}
                     <SubmitSurvey
