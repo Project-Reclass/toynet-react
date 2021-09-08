f2274227b4cfe2c5129fc003dc352d18b9f91257
Scott Richardson <scottjr632@gmail.com>
2021-09-08 13:52:06 -0500

    Persist Node Locations (#102)

--- src/__tests__/Emulator/Visuals/Flow/Flow.test.jsx
+++ src/__tests__/Emulator/Visuals/Flow/Flow.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { waitFor } from '@testing-library/react';
 import { renderWithTheme } from "src/common/test-utils/renderWithTheme";
 import Flow from "src/Emulator/Visuals/Flow";
 
@@ -33,46 +33,56 @@ const hosts = [
 ];
 
 describe('Flow', () => {
-  it('should match the snapshot', () => {
+  it('should match the snapshot', async () => {
     const { container, getByText } = renderWithTheme(<Flow
       switches={switches}
       hosts={hosts}
       routers={routers}
       isTesting={true}
     />);
+
+    await waitFor(() => getByText(/r1/gi));
+
     expect(getByText(/r1/gi)).toBeInTheDocument();
     expect(getByText(/r2/gi)).toBeInTheDocument();
     expect(getByText(/s1/gi)).toBeInTheDocument();
     expect(getByText(/h1/gi)).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
-  it('should be able to render only hosts', () => {
+  it('should be able to render only hosts', async () => {
     const { container, getByText } = renderWithTheme(<Flow
       switches={[]}
       routers={[]}
       hosts={hosts}
       isTesting={true}
     />);
+    
+    await waitFor(() => getByText(/h1/gi));
     expect(getByText(/h1/gi)).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
-  it('should be able to render only switches', () => {
+  it('should be able to render only switches', async () => {
     const { container, getByText } = renderWithTheme(<Flow
       switches={switches}
       routers={[]}
       hosts={[]}
       isTesting={true}
     />);
+
+    await waitFor(() => getByText(/s1/gi));
+
     expect(getByText(/s1/gi)).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
-  it('should be able to render only routers', () => {
+  it('should be able to render only routers', async () => {
     const { container, getByText } = renderWithTheme(<Flow
       switches={[]}
       routers={routers}
       hosts={[]}
       isTesting={true}
     />);
+
+    await waitFor(() => getByText(/r1/gi));
     expect(getByText(/r1/gi)).toBeInTheDocument();
     expect(getByText(/r2/gi)).toBeInTheDocument();
     expect(container).toMatchSnapshot();
