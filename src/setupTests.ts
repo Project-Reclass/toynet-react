3c9c507791baea5488c349e52b6afbb630833205
Scott Richardson <scottjr632@gmail.com>
2021-09-09 12:05:56 -0500

    Setup Open Source (#96)
    * Creates LICENSE
    * Adds header check for LICENSS

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,23 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/*
+Copyright (C) 1992-2021 Free Software Foundation, Inc.
+
+This file is part of ToyNet React.
+
+ToyNet React is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+ToyNet React is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ToyNet React; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
+
+*/
 import '@testing-library/jest-dom/extend-expect';
 
 import { ResizeObserver } from '@juggle/resize-observer';
