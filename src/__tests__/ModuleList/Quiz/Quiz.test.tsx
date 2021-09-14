fdab1e733a9e7fcce0ca1a8bba47a60c3c10ac5a
Shaili Smith <shailismith@Shailis-MacBook-Air.local>
2021-09-14 13:40:40 -0500

    Fixing ESLint errors and running tests

--- src/__tests__/ModuleList/Quiz/Quiz.test.tsx
+++ src/__tests__/ModuleList/Quiz/Quiz.test.tsx
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 1992-2021 Free Software Foundation, Inc.
+
+This file is part of ToyNet React.
+
+ToyNet React is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+ToyNet React is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ToyNet React; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
+
+*/
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { fireEvent, getByTestId, waitFor, screen } from '@testing-library/react';
@@ -15,35 +35,36 @@ const RenderWithRouter = ({ children, moduleId, quizId }: {children: React.React
   </MemoryRouter>
 );
 
+const ANSWER: number = 3;
 const data = [
   {
-    "question": "How many nodes share a single channel on a bus topology?",
-    "options": [
-        "One node",
-        "Only ones communicating",
-        "Three nodes",
-        "All nodes"
+    'question': 'How many nodes share a single channel on a bus topology?',
+    'options': [
+        'One node',
+        'Only ones communicating',
+        'Three nodes',
+        'All nodes',
     ],
-    "answer": 3
+    'answer': ANSWER,
   },
   {
-    "question": "Which of the following is the most fault-tolerant WAN topology?",
-    "options": [
-        "Partial Mesh",
-        "Ring",
-        "Star",
-        "Full-mesh"
+    'question': 'Which of the following is the most fault-tolerant WAN topology?',
+    'options': [
+        'Partial Mesh',
+        'Ring',
+        'Star',
+        'Full-mesh',
     ],
-    "answer": 3
-  }
+    'answer': ANSWER,
+  },
 ];
 describe('The Quiz page', () => {
-  
+
   it('should render the same based on URL parameters', () => {
     const { container } = renderWithTheme(
       <RenderWithRouter moduleId={42} quizId={64}>
         <Quiz />
-      </RenderWithRouter>
+      </RenderWithRouter>,
     );
     expect(container).toMatchSnapshot();
   });
@@ -54,7 +75,7 @@ describe('The Quiz page', () => {
     const { getByText, getAllByText } = renderWithTheme(
       <RenderWithRouter moduleId={42} quizId={64}>
         <Quiz />
-      </RenderWithRouter>
+      </RenderWithRouter>,
     );
 
     await waitFor(() => getAllByText(/How many nodes/i));
@@ -63,42 +84,42 @@ describe('The Quiz page', () => {
     expect(getByText(/fault-tolerant WAN topology/i)).toBeInTheDocument();
     expect(getByText(/Partial Mesh/i)).toBeInTheDocument();
   });
-  
-  it('should err if none answered',() => {
+
+  it('should err if none answered', () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
     getQuizMetaMock.mockResolvedValue({ items: data });
     const { getByText, getAllByText } = renderWithTheme(
       <RenderWithRouter moduleId={42} quizId={64}>
         <Quiz />
-      </RenderWithRouter>
+      </RenderWithRouter>,
     );
     const submitBtn = getByText(/submit/i);
     fireEvent.click(submitBtn);
     expect(window.alert).toBeCalled();
-    
-  })
-  it('should err if not all answered',() => {
+
+  });
+  it('should err if not all answered', () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
     getQuizMetaMock.mockResolvedValue({ items: data });
     const { getByText, getAllByText } = renderWithTheme(
       <RenderWithRouter moduleId={42} quizId={64}>
         <Quiz />
-      </RenderWithRouter>
+      </RenderWithRouter>,
     );
     const choice1 = screen.getByTestId('Only ones communicating');
     const submitBtn = getByText(/submit/i);
     fireEvent.click(choice1);
     fireEvent.click(submitBtn);
     expect(window.alert).toBeCalled();
-    
-  })
-  it('should not err if all answered',() => {
+
+  });
+  it('should not err if all answered', () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
     getQuizMetaMock.mockResolvedValue({ items: data });
     const { getByText, getAllByText } = renderWithTheme(
       <RenderWithRouter moduleId={42} quizId={64}>
         <Quiz />
-      </RenderWithRouter>
+      </RenderWithRouter>,
     );
     const choice1 = screen.getByTestId('Only ones communicating');
     const choice2 = screen.getByTestId('Partial Mesh');
@@ -107,7 +128,7 @@ describe('The Quiz page', () => {
     fireEvent.click(choice2);
     fireEvent.click(submitBtn);
     expect(window.alert).not.toBeCalled();
-  
-  })
-  
+
+  });
+
 });
