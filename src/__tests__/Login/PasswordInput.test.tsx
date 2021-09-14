a072a1d177d70019375d284b82b721ac3cbb0259
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2021-09-14 14:16:45 -0400

    run ESLint

--- src/__tests__/Login/PasswordInput.test.tsx
+++ src/__tests__/Login/PasswordInput.test.tsx
@@ -19,35 +19,35 @@ along with ToyNet React; see the file LICENSE.  If not see
 
 */
 
-import React from "react";
-import { act, fireEvent } from "@testing-library/react";
-import { renderWithTheme } from "src/common/test-utils/renderWithTheme";
+import React from 'react';
+import { act, fireEvent } from '@testing-library/react';
+import { renderWithTheme } from 'src/common/test-utils/renderWithTheme';
 
-import PasswordInput from "src/Login/PasswordInput";
+import PasswordInput from 'src/Login/PasswordInput';
 
-describe("The password input field", () => {
-  it("should match snapshots", () => {
+describe('The password input field', () => {
+  it('should match snapshots', () => {
     const { container } = renderWithTheme(<PasswordInput />);
     expect(container).toMatchSnapshot();
   });
-  it("should match snapshot with text hidden by default", () => {
+  it('should match snapshot with text hidden by default', () => {
     const { getByTestId, container } = renderWithTheme(<PasswordInput />);
-    const input = getByTestId("password-input");
+    const input = getByTestId('password-input');
     act(() => {
-      fireEvent.change(input, { target: { value: "test" } });
+      fireEvent.change(input, { target: { value: 'test' } });
     });
     expect(container).toMatchSnapshot();
   });
-  it("should match snapshot with text shown", () => {
+  it('should match snapshot with text shown', () => {
     const { getByTestId, getByText, container } = renderWithTheme(
-      <PasswordInput />
+      <PasswordInput />,
     );
 
     const btn = getByText(/show/i);
-    const input = getByTestId("password-input");
+    const input = getByTestId('password-input');
 
     act(() => {
-      fireEvent.change(input, { target: { value: "test" } });
+      fireEvent.change(input, { target: { value: 'test' } });
     });
     act(() => {
       fireEvent.click(btn);
@@ -56,7 +56,7 @@ describe("The password input field", () => {
     expect(getByText(/hide/i)).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
-  it("should have a button that toggles from show / hide", () => {
+  it('should have a button that toggles from show / hide', () => {
     const { getByText } = renderWithTheme(<PasswordInput />);
 
     const btn = getByText(/show/i);
