2d0817e8caad38906b1becf3bb55a15b6e9c0322
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-09-14 20:58:41 +0200

    Update QuizModal.test.tsx

--- src/__tests__/ModuleList/Quiz/QuizModal.test.tsx
+++ src/__tests__/ModuleList/Quiz/QuizModal.test.tsx
@@ -1,7 +1,31 @@
+/*
+Copyright (C) 1992-2021 Free Software Foundation, Inc.
+
+This file is part of ToyNet React.
+
+ToyNet React is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+ToyNet React is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ToyNet React; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
+
+*/
+
+/* eslint-disable no-magic-numbers */
+
+
 import React from 'react';
 
-import { renderWithTheme } from "src/common/test-utils/renderWithTheme";
-import QuizModal from "src/Curriculum/Quiz/QuizModal";
+import { renderWithTheme } from 'src/common/test-utils/renderWithTheme';
+import QuizModal from 'src/Curriculum/Quiz/QuizModal';
 
 const defaultProps = {
     isOpen: true,
@@ -9,15 +33,15 @@ const defaultProps = {
     total: 10,
     done: () => null,
     tryAgain: () => null,
-}
+};
 
-const renderHelper = ({...props}) => (
+const renderHelper = ({...props}:{ isOpen: boolean, numCorrect: number, total: number, done: () => null, tryAgain: () => null}) => (
   renderWithTheme(
     <div>
       <QuizModal {...props} />
-    </div>
+    </div>,
   )
-)
+);
 
 describe('The quiz score modal', () => {
   it('should show a score', () => {
@@ -26,12 +50,12 @@ describe('The quiz score modal', () => {
   });
   it('should only show a try again button when score is under 70%', () => {
     const { queryAllByText, getByText } = renderHelper({...defaultProps, numCorrect: 0});
-    expect(getByText('Try Again')); // Find "Try Again" button with exact text match
-    expect(queryAllByText(`I'm Done`)).toHaveLength(0); // There should be no "I'm Done" button
+    expect(getByText('Try Again')).toBeInTheDocument(); // Find "Try Again" button with exact text match
+    expect(queryAllByText('I\'m Done')).toHaveLength(0); // There should be no "I'm Done" button
   });
   it('should show a `done` button and try again when score is 70% or over', () => {
-    const { getByText , queryAllByText} = renderHelper({...defaultProps, numCorrect: 7});
+    const { getByText, queryAllByText} = renderHelper({...defaultProps, numCorrect: 7});
     expect(getByText('Try Again')).toBeInTheDocument(); // Try again should still be visible
-    expect(queryAllByText(`I'm Done`)).toHaveLength(1); // The done button should also now be visible
-  })
-})
+    expect(queryAllByText('I\'m Done')).toHaveLength(1); // The done button should also now be visible
+  });
+});
