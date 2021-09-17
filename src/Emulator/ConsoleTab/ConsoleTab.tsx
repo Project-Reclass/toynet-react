3fa6c8702cda5bff8d81ed8f9a515926e8e0f1fc
Scott Richardson <scottjr632@gmail.com>
2021-09-16 19:15:44 -0500

    Initial commit

--- src/Emulator/ConsoleTab/ConsoleTab.tsx
+++ src/Emulator/ConsoleTab/ConsoleTab.tsx
@@ -18,21 +18,97 @@ along with ToyNet React; see the file LICENSE.  If not see
 <http://www.gnu.org/licenses/>.
 
 */
-import React from 'react';
+import { Box, Flex, Heading, Select, Text, Textarea } from '@chakra-ui/core';
+import React, { useState } from 'react';
 
 import {
   EmulatorInnerSection,
   EmulatorSection,
-  EmulatorTitle,
 } from 'src/common/components/Emulator';
 
-const ConsoleTab = () => (
-  <EmulatorSection>
-    <EmulatorTitle size='lg' color='white'>
-      Console
-    </EmulatorTitle>
-    <EmulatorInnerSection></EmulatorInnerSection>
-  </EmulatorSection>
+interface ToynetCommand {
+  command: string;
+  output: string;
+  color: string;
+}
+
+const AppliedCommand = ({command, color, output}: ToynetCommand) => (
+  <Box>
+    <Text>{`${command}`}</Text>
+    <Text color={color}>{output}</Text>
+  </Box>
 );
+
+const ConsoleTab = () => {
+  const [currInput, setCurrInput] = useState('>> ');
+  const [history, setHistory] = useState<ToynetCommand[]>([
+    {
+      command: '>> ls',
+      output: 'name',
+      color: 'white',
+    },
+  ]);
+
+  return (
+    <EmulatorSection>
+      <Flex paddingBottom='0.559rem' justifyContent='space-between'>
+        <Heading size='lg' color='white'>
+          Console
+        </Heading>
+        <Flex width='fit-content' height='fit-content'>
+          <Text my='auto' marginRight='1rem'>Device</Text>
+          <Select
+            size='sm'
+            color='white'
+            backgroundColor='#212529'
+            placeholder='Select device'
+            width='fit-content'
+            borderWidth='1'
+            borderRadius={3}
+          >
+            <option>H1</option>
+          </Select>
+        </Flex>
+      </Flex>
+      <EmulatorInnerSection padding='1rem'>
+        {history.map(cmd => (
+          <AppliedCommand {...cmd} />
+        ))}
+        <Textarea
+          _hover={{ borderColor: 'rgba(0,0,0,0)' }}
+          padding='0'
+          focusBorderColor='rgba(0,0,0,0)'
+          borderColor='rgba(0,0,0,0)'
+          backgroundColor='rgba(0,0,0,0)'
+          resize='none'
+          value={currInput}
+          height='fit-content'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              e.preventDefault();
+              const { value } = e.currentTarget;
+              if (value.length > 2 && !value.endsWith('\n'))
+                setCurrInput(`${e.currentTarget.value}`);
+            }
+          }
+          onKeyPress={(e: React.KeyboardEvent) => {
+            if (e.shiftKey && e.key === 'Enter') {
+              setCurrInput(curr => `${curr}\n`);
+              return;
+            }
+            if (e.key === 'Enter') {
+              setHistory(prev => [...prev, {
+                command: currInput,
+                output: 'ls',
+                color: 'tomato',
+              }]);
+              setCurrInput('>> ');
+              console.log('pressed enter');
+            }
+          }}
+        />
+      </EmulatorInnerSection>
+    </EmulatorSection>
+  );
+};
 
 export default ConsoleTab;
