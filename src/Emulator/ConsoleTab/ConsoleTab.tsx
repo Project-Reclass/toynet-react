6cc342d21a8fafc8a411526143401fba81f2b67f
Scott Richardson <scottjr632@gmail.com>
2021-09-28 18:37:40 -0500

    wip:

--- src/Emulator/ConsoleTab/ConsoleTab.tsx
+++ src/Emulator/ConsoleTab/ConsoleTab.tsx
@@ -18,61 +18,73 @@ along with ToyNet React; see the file LICENSE.  If not see
 <http://www.gnu.org/licenses/>.
 
 */
-import { Box, Flex, Heading, Skeleton, Text, Textarea } from '@chakra-ui/core';
-import React, { memo, useEffect, useMemo, useRef, useState } from 'react';
+
+import React, { memo, useEffect, useRef, useState } from 'react';
+import { Box, Flex, Heading, Text, Textarea } from '@chakra-ui/core';
 import { useToynetCommand } from 'src/common/api/topology';
 import { ToynetCommandResponse } from 'src/common/api/topology/types';
-
 import {
   EmulatorInnerSection,
   EmulatorSection,
 } from 'src/common/components/Emulator';
+
 import { useEmulator } from '../EmulatorProvider';
-import DeviceSelector from './DeviceSelector';
 
 interface ToynetCommand {
   command: string;
   output: string;
   color: string;
+  created: string;
 }
 
-const AppliedCommand = ({command, color, output}: ToynetCommand) => (
+const AppliedCommand = memo(({command, color, output}: ToynetCommand) => (
   <Box>
     <Text>{`${command}`}</Text>
     <Text color={color}>{output}</Text>
   </Box>
-);
+));
 
-const CoolHeading = memo(() => (
+const ConsoleHeading = memo(() => (
   <Heading size='lg'>
     Console
   </Heading>
 ));
 
 const ConsoleTab = () => {
-  const { switches, hosts, routers, sessionId, isLoading } = useEmulator();
-  const [runCommand, { isError, isIdle }] = useToynetCommand(sessionId);
+  const { sessionId, isLoading } = useEmulator();
+  const [runCommand, { isError, isIdle, error }] = useToynetCommand(sessionId);
+  const errorRef = useRef(error);
 
   const scrollRef = useRef<HTMLDivElement>(null);
   const [currInput, setCurrInput] = useState('>> ');
   const [history, setHistory] = useState<ToynetCommand[]>([]);
 
   useEffect(() => {
-    if (!isIdle && isError) {
-      setCurrInput(curr => {
-        // eslint-disable-next-line max-nested-callbacks
-        setHistory(prev => [
-          ...prev,
-          {
-            command: curr,
-            output: 'Error',
-            color: 'tomato',
-          },
-        ]);
-        return '>> ';
-      });
-    }
-  }, [isError, isIdle, isLoading]);
+    if (!error || error === errorRef.current)
+      return;
+
+    errorRef.current = error;
+    console.log('cool cool cool');
+    const updateHistory = (command: string) => {
+      console.log('updateing history');
+      setHistory(prev => [
+        ...prev,
+        {
+          command,
+          output: 'Error',
+          color: 'tomato',
+          created: `${new Date().toISOString()}${Math.random()}effect`,
+        },
+      ]);
+    };
+
+    setCurrInput(curr => {
+      if (curr !== '>> ')
+        updateHistory(curr);
+      return '>> ';
+    });
+
+  }, [error]);
 
   useEffect(() => {
     if (scrollRef.current) {
@@ -81,81 +93,70 @@ const ConsoleTab = () => {
     }
   }, [history]);
 
-  const options = useMemo(() =>
-    [
-      ...routers.map(el => el.name),
-      ...switches.map(el => el.name),
-      ...hosts.map(el => el.name),
-    ],
-  [hosts, routers, switches]);
-
   const handleSubmit = async () => {
     const res = await runCommand(currInput.replace('>> ', ''));
     if (!res)
       return;
 
+    console.log('submit', {history});
     setHistory(prev => [
       ...prev,
       {
-        ...res as ToynetCommandResponse,
+        ...res,
         command: currInput,
-        color: 'green.200',
+        color: 'grey',
+        created: `${new Date().toISOString()}${Math.random()}error`,
       },
     ]);
     setCurrInput('>> ');
   };
 
   return (
-    <EmulatorSection
-      overflow='hidden'
-    >
+    <EmulatorSection overflow='hidden'>
       <Flex paddingBottom='0.559rem' justifyContent='space-between'>
-        <CoolHeading />
-        <DeviceSelector options={options} />
+        <ConsoleHeading />
       </Flex>
-      <Skeleton isLoaded={!isLoading} width='100%' height='100%'  colorStart='#22252c'
-            colorEnd='#454950'>
-        <EmulatorInnerSection
-          ref={scrollRef}
-          padding='1rem'
-        >
-          {history.map(cmd => (
-            <AppliedCommand {...cmd} />
-          ))}
-          <Textarea
-            _hover={{ borderColor: 'rgba(0,0,0,0)' }}
-            padding='0'
-            focusBorderColor='rgba(0,0,0,0)'
-            borderColor='rgba(0,0,0,0)'
-            backgroundColor='rgba(0,0,0,0)'
-            resize='none'
-            value={currInput}
-            height='fit-content'
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                e.preventDefault();
-                const { value } = e.currentTarget;
-                if (value.length > 2 && !value.endsWith('\n'))
-                  setCurrInput(`${e.currentTarget.value}`);
-              }
+      <EmulatorInnerSection
+        ref={scrollRef}
+        padding='1rem'
+      >
+        {console.log({ history })}
+        {history.map(cmd => (
+          <AppliedCommand key={cmd.created} {...cmd} />
+        ))}
+        <Textarea
+          _hover={{ borderColor: 'rgba(0,0,0,0)' }}
+          padding='0'
+          focusBorderColor='rgba(0,0,0,0)'
+          borderColor='rgba(0,0,0,0)'
+          backgroundColor='rgba(0,0,0,0)'
+          resize='none'
+          value={currInput}
+          height='fit-content'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              e.preventDefault();
+              const { value } = e.currentTarget;
+              if (value.length > 2 && !value.endsWith('\n'))
+                setCurrInput(`${e.currentTarget.value}`);
             }
-            onKeyPress={(e: React.KeyboardEvent) => {
-              if (e.shiftKey && e.key === 'Enter') {
-                setCurrInput(curr => `${curr}\n`);
+          }
+          onKeyPress={(e: React.KeyboardEvent) => {
+            if (e.shiftKey && e.key === 'Enter') {
+              setCurrInput(curr => `${curr}\n`);
+              return;
+            }
+            if (e.key === 'Enter') {
+              if (currInput.replace('>> ', '') === 'clear') {
+                setCurrInput('>> ');
+                setHistory([]);
                 return;
               }
-              if (e.key === 'Enter') {
-                if (currInput.replace('>> ', '') === 'clear') {
-                  setCurrInput('>> ');
-                  setHistory([]);
-                  return;
-                }
 
-                handleSubmit();
-              }
-            }}
-          />
-        </EmulatorInnerSection>
-      </Skeleton>
+              handleSubmit();
+            }
+          }}
+        />
+      </EmulatorInnerSection>
     </EmulatorSection>
   );
 };
