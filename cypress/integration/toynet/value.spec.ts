c3a8fb3e6ec37f4340625e0298a97fcd6ae276a8
Scott Richardson <scottjr632@gmail.com>
2021-10-14 16:04:21 -0500

    Adds value e2e spec

---
+++ cypress/integration/toynet/value.spec.ts
@@ -0,0 +1,30 @@
+/*
+Copyright (C) 1992-2021 Free Software Foundation, Inc.
+
+This file is part of ToyNet React.
+
+ToyNet React is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+ToyNet React is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ToyNet React; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
+
+*/
+
+describe('The value page', () => {
+  it('should value 5001', () => {
+    cy.visit('http://localhost:3000/value/5001');
+    cy.contains(/integrity/i).should('be.visible');
+    cy.contains('U.S. Air Force').should('be.visible');
+    cy.contains('U.S. Army').should('be.visible');
+    cy.contains(/save/i).scrollIntoView().should('be.visible');
+  });
+});
