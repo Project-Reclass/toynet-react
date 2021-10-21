78b501612280f2106469cbd48086f8a0d3a31b24
Scott Richardson <scottjr632@gmail.com>
2021-10-21 12:29:53 -0500

    Fixes issue where message could be overwritten

--- src/common/providers/EmulatorProvider.tsx
+++ src/common/providers/EmulatorProvider.tsx
@@ -18,7 +18,7 @@ along with ToyNet React; see the file LICENSE.  If not see
 <http://www.gnu.org/licenses/>.
 
 */
-import React, { createContext, useContext, FC, useCallback, useState, useEffect } from 'react';
+import React, { createContext, useContext, FC, useCallback, useEffect, useRef } from 'react';
 import { useParams } from 'react-router';
 import { useSessionStorage } from 'src/common/hooks/useSessionStorage';
 
@@ -46,28 +46,36 @@ const DialogueContext = createContext<DialogueInterface>({
 });
 
 const DialogueProvider: FC = ({ children }) => {
-  const [queue, setQueue] = useState<DialogueMessage[]>([]);
   const [dialogueMessages, setDialogueMessages, isInitialized] =
     useSessionStorage<DialogueMessage[]>('history', [],
       value => JSON.parse(value));
 
+  // We need this queue because there could be instances where a message
+  // is added to the dialogue but we haven't loaded the saved messages
+  // from session storage. If that happens, then our messages in session
+  // storage will get erased. We can use this to to store those messages
+  // added before initialization and then add it later.
+  const queue = useRef<DialogueMessage[]>([]);
+
   useEffect(() => {
     if (isInitialized) {
-      setDialogueMessages(msgs => [...msgs, ...queue]);
-      setQueue([]);
+      setDialogueMessages(msgs => [...msgs, ...queue.current]);
+      queue.current = [];
     }
-  }, [isInitialized, queue, setDialogueMessages]);
-
+  }, [isInitialized, setDialogueMessages]);
 
   // Not using useCallback so we can add the same error messages repeatedly
   const appendDialogue = useCallback((message: string, color = 'White'): DialogueMessageId => {
     const id = genUniqueId();
+
+    // if the history has not been grabbed from session storage yet, we need to
+    // store that message in the queue to be added on after initialization.
     if (!isInitialized) {
-      setQueue(prev => [...prev, { id, message, color }]);
+      queue.current.push({ id, message, color });
       return id;
     }
 
-    setDialogueMessages(prev => [...prev, {id, message, color}]);
+    setDialogueMessages(prev => [...prev, { id, message, color }]);
     return id;
   }, [isInitialized, setDialogueMessages]);
 
@@ -81,6 +89,7 @@ const DialogueProvider: FC = ({ children }) => {
   ) => {
     const updateMessage = (messages: DialogueMessage[]) => {
       const messageToUpdate = messages.findIndex(message => message.id === id);
+      console.log({ messageToUpdate, messages });
       if (messageToUpdate === -1)
         return messages;
 
