b5356d6c81ca2c5443f189468375881bc7a48c0d
Scott Richardson <scottjr632@gmail.com>
2021-11-02 17:19:42 -0500

    Creates useFirstRender hook

---
+++ src/common/hooks/useFirstRender.ts
@@ -0,0 +1,31 @@
+/*
+Copyright (C) 1992-2021 Free Software Foundation, Inc.
+
+This file is part of ToyNet React.
+
+ToyNet React is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+ToyNet React is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with ToyNet React; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
+
+*/
+import { useRef, useEffect } from 'react';
+
+export default function useFirstRender() {
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    isFirstRender.current = false;
+  }, []);
+
+  return isFirstRender.current;
+}
