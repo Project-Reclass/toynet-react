3ab9d8252ecdad3556287185891900d4bb29a5f5
Scott Richardson <scottjr632@gmail.com>
2022-10-09 11:40:55 -0700

    Add better debug for docker entrypoint for nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,11 @@ defaultCurrURI=https://raw.githubusercontent.com/Project-Reclass/toynet-content/
 
 if [ -z ${SERVER_URI+x} ]; then echo "SERVER_URI needs to be set"; exit 1 ; else echo "SERVER_URI is set to '$SERVER_URI'"; fi
 
-if [ -z ${CURRICULUM_URI} ]; then echo "CURRICULUM_URI not set. Defaulting to $defaultCurrURI"; export CURRICULUM_URI=${defaultCurrURI}; else echo "CURRICULUM_URI is et to '$defaultCurrURI'"; fi
+if [ -z ${CURRICULUM_URI+x} ]; then echo "CURRICULUM_URI not set. Defaulting to $defaultCurrURI"; export CURRICULUM_URI=${defaultCurrURI}; else echo "CURRICULUM_URI is et to '$CURRICULUM_URI'"; fi
 
 envsubst '\${CURRICULUM_URI},\${SERVER_URI}' < http.conf > /etc/nginx/conf.d/default.conf
+
+cat /etc/nginx/conf.d/default.conf
 
 nginx -t
 exec nginx -g 'daemon off;'
